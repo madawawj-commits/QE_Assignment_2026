a7afed845ef5da91bda35994dc01fa5b4b46c9ba
prasadM01 <madawa.prasad@shorelight.com>
2026-04-22 21:15:52 +0530

    Feat:pordcut view scenario

--- surge-playwright-test-automation-framework/tests/web/fixtures/basePage.ts
+++ surge-playwright-test-automation-framework/tests/web/fixtures/basePage.ts
@@ -1,8 +1,9 @@
 import { test as base } from '@playwright/test';
 import LoginPage from '../../../pageObjects/pages/login.page';
 import LogoutPage from '../../../pageObjects/pages/logout.page';
+import ProductsPage from '../../../pageObjects/pages/products.page';
 
-export const test = base.extend<{ loginPage: LoginPage; logoutPage: LogoutPage }>(
+export const test = base.extend<{ loginPage: LoginPage; logoutPage: LogoutPage; productsPage: ProductsPage }>(
     {
         //Define fixture
         loginPage: async ({ page }, use) => {
@@ -10,6 +11,9 @@ export const test = base.extend<{ loginPage: LoginPage; logoutPage: LogoutPage }
         },
         logoutPage: async ({ page }, use) => {
             await use(new LogoutPage(page))
+        },
+        productsPage: async ({ page }, use) => {
+            await use(new ProductsPage(page))
         },
     }
 )
